52e113d624ea1cdb09f731803fa6891b000fcb37
Neonila Shlikhtenko <shlikhtenkoneonila@gmail.com>
2014-03-02 15:13:19 +0200

    Complete popup

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 $(function() {
-	($(".j-question")).on("click", function (e) {
+	$(".j-question").on("click", function (e) {
 		e.preventDefault();
 		var $faq = $(this).closest("li");
 		$faq.toggleClass("is-open").find(".answer").slideToggle(100);
-	})
+	});
+
+	$(".j-btn-close").on("click", function (e) {
+		e.preventDefault();
+		$(this).closest(".popup").hide();
+	});
+
+	$(".j-btn-ask").on("click", function (e) {
+		e.preventDefault();
+		$(".popup").show();
+	});
 });
